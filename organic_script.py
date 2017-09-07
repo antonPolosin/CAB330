604e1fa3c4214c9ea0a6ddd6d1ef8bebda3f2c4e
Nagarius <pooper2010@live.com.au>
2017-09-07 17:32:29 +1000

    Completed

--- organic_script.py
+++ organic_script.py
@@ -18,8 +18,9 @@ from sklearn.feature_selection import RFECV
 from sklearn.decomposition import PCA
 from sklearn.feature_selection import SelectFromModel
 # Task.4.
-
 from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
 
 # start of script
 def data_prep():
@@ -254,7 +255,7 @@ def rf_regression():
 	
 	# split data into 70% training data and 30% test data
 	X_mat = X.as_matrix()
-	X_train, X_test, y_train, y_test = train_test_split(X_mat, y, test_size=0.7, random_state=42, stratify=y)
+	X_train, X_test, y_train, y_test = train_test_split(X_mat, y, test_size=0.33, random_state=42, stratify=y)
 	
 	# scaling input values because of outlier data
 	scaler = StandardScaler()
@@ -313,7 +314,7 @@ def pc_regression():
 			print("N components with > 95% variance =", idx+1)
 			break
 	
-	pca = PCA(n_components=53)
+	pca = PCA(n_components=52)
 	X_train_pca = pca.fit_transform(X_train)
 	X_test_pca = pca.transform(X_test)
 	
@@ -397,7 +398,7 @@ def neural_network():
 	X_train = scaler.fit_transform(X_train, y_train)
 	X_test = scaler.transform(X_test)
 	
-	model = MLPClassifier(max_iter=500)
+	model = MLPClassifier()
 	model.fit(X_train, y_train)
 	
 	print("Train accuracy:", model.score(X_train, y_train))
@@ -408,6 +409,247 @@ def neural_network():
 	
 	print(model)
 	
+def cv_neural_network():
+	df = data_prep()
+	
+	# split into y as target variable and X as input variable
+	y = df['ORGYN']
+	X = df.drop(['ORGYN'], axis=1)
+	
+	# split data into 70% training data and 30% test data
+	X_mat = X.as_matrix()
+	X_train, X_test, y_train, y_test = train_test_split(X_mat, y, test_size=0.7, random_state=42, stratify=y)
+	
+	# scaling input values because of outlier data
+	scaler = StandardScaler()
+	X_train = scaler.fit_transform(X_train, y_train)
+	X_test = scaler.transform(X_test)
+	print(X_train.shape)
+	
+	#With 52 features, we will start tuning with one hidden layer of 5 to 52 neurons, increment of 25. 
+	params = {'hidden_layer_sizes': [(x,) for x in range(5, 53, 15)]}
+
+	cv = GridSearchCV(param_grid=params, estimator=MLPClassifier(max_iter=1000), cv=10, n_jobs=-1)
+	cv.fit(X_train, y_train)
+
+	print("Train accuracy:", cv.score(X_train, y_train))
+	print("Test accuracy:", cv.score(X_test, y_test))
+
+	y_pred = cv.predict(X_test)
+	print(classification_report(y_test, y_pred))
+
+	print(cv.best_params_)
+	
+	#GridSearchCV returns 5 neurons . For this dataset more complex models tend to overfit. tune lower neuron count.
+	params = {'hidden_layer_sizes': [(3,), (5,), (7,), (9,)]}
+
+	cv = GridSearchCV(param_grid=params, estimator=MLPClassifier(max_iter=1000), cv=10, n_jobs=-1)
+	cv.fit(X_train, y_train)
+
+	print("Train accuracy:", cv.score(X_train, y_train))
+	print("Test accuracy:", cv.score(X_test, y_test))
+
+	y_pred = cv.predict(X_test)
+	print(classification_report(y_test, y_pred))
+
+	print(cv.best_params_)
+	
+	#Neoron count returned as 5, tune alpha
+	# default value is 0.0001, tune around
+	
+	params = {'hidden_layer_sizes': [(3,), (5,), (7,), (9,)], 'alpha': [0.01,0.001, 0.0001, 0.00001]}
+
+	cv = GridSearchCV(param_grid=params, estimator=MLPClassifier(max_iter=1000), cv=10, n_jobs=-1)
+	cv.fit(X_train, y_train)
+
+	print("Train accuracy:", cv.score(X_train, y_train))
+	print("Test accuracy:", cv.score(X_test, y_test))
+
+	y_pred = cv.predict(X_test)
+	print(classification_report(y_test, y_pred))
+
+	print(cv.best_params_)
+
+def rfr_neural_network():
+	df = data_prep()
+	
+	# split into y as target variable and X as input variable
+	y = df['ORGYN']
+	X = df.drop(['ORGYN'], axis=1)
+	
+	# split data into 70% training data and 30% test data
+	X_mat = X.as_matrix()
+	X_train, X_test, y_train, y_test = train_test_split(X_mat, y, test_size=0.7, random_state=42, stratify=y)
+	
+	# scaling input values because of outlier data
+	scaler = StandardScaler()
+	X_train = scaler.fit_transform(X_train, y_train)
+	X_test = scaler.transform(X_test)
+	
+	rfe = RFECV(estimator = LogisticRegression(), cv=10)
+	rfe.fit(X_train, y_train)
+	
+	X_train_rfe = rfe.transform(X_train)
+	X_test_rfe = rfe.transform(X_test)
+	
+
+	# step = int((X_train_rfe.shape[1] + 5)/5);
+	params = {'hidden_layer_sizes': [(3,), (5,), (7,), (9,)], 'alpha': [0.01,0.001, 0.0001, 0.00001]}
+
+	cv = GridSearchCV(param_grid=params, estimator=MLPClassifier(max_iter=1000), cv=10, n_jobs=-1)
+	cv.fit(X_train_rfe, y_train)
+	
+
+	print("Train accuracy:", cv.score(X_train_rfe, y_train))
+	print("Test accuracy:", cv.score(X_test_rfe, y_test))
+
+	y_pred = cv.predict(X_test_rfe)
+	print(classification_report(y_test, y_pred))
+
+	print(cv.best_params_)
+
+	print(rfe.n_features_)
+	
+def rfdt_neural_network():
+	df = data_prep()
+	
+	# split into y as target variable and X as input variable
+	y = df['ORGYN']
+	X = df.drop(['ORGYN'], axis=1)
+	
+	# split data into 70% training data and 30% test data
+	X_mat = X.as_matrix()
+	X_train, X_test, y_train, y_test = train_test_split(X_mat, y, test_size=0.7, random_state=42, stratify=y)
+	
+	# scaling input values because of outlier data
+	scaler = StandardScaler()
+	X_train = scaler.fit_transform(X_train, y_train)
+	X_test = scaler.transform(X_test)
+	
+	rfe = RFECV(estimator = DecisionTreeClassifier(), cv=10)
+	rfe.fit(X_train, y_train)
+	
+	X_train_rfe = rfe.transform(X_train)
+	X_test_rfe = rfe.transform(X_test)
+	
+
+	# step = int((X_train_rfe.shape[1] + 5)/5);
+	params = {'hidden_layer_sizes': [(3,), (5,), (7,), (9,)], 'alpha': [0.01,0.001, 0.0001, 0.00001]}
+
+	cv = GridSearchCV(param_grid=params, estimator=MLPClassifier(max_iter=1000), cv=10, n_jobs=-1)
+	cv.fit(X_train_rfe, y_train)
+	
+
+	print("Train accuracy:", cv.score(X_train_rfe, y_train))
+	print("Test accuracy:", cv.score(X_test_rfe, y_test))
+
+	y_pred = cv.predict(X_test_rfe)
+	print(classification_report(y_test, y_pred))
+
+	print(cv.best_params_)
+
+	print(rfe.n_features_)
+
+########################### Comparing models ##########################################
+def compare():
+	df = data_prep()
+	
+	# split into y as target variable and X as input variable
+	y = df['ORGYN']
+	X = df.drop(['ORGYN'], axis=1)
+	
+	# split data into 70% training data and 30% test data
+	X_mat = X.as_matrix()
+	X_train, X_test, y_train, y_test = train_test_split(X_mat, y, test_size=0.7, random_state=42, stratify=y)
+	
+	# scaling input values because of outlier data
+	scaler = StandardScaler()
+	X_train = scaler.fit_transform(X_train, y_train)
+	X_test = scaler.transform(X_test)
+	
+	# grid search CV for decision tree
+	params_dt = {'criterion': ['gini'],
+			  'max_depth': range(2, 5),
+			  'min_samples_leaf': range(40, 61, 5)}
+
+	cv = GridSearchCV(param_grid=params_dt, estimator=DecisionTreeClassifier(), cv=10)
+	cv.fit(X_train, y_train)
+
+	dt_model = cv.best_estimator_
+	print(dt_model)
+
+	# hyperparameters for logistic regression
+	params_log_reg = {'C': [pow(10, x) for x in range(-6, 4)]}
+
+	cv = GridSearchCV(param_grid=params_log_reg, estimator=LogisticRegression(), cv=10, n_jobs=-1)
+	cv.fit(X_train, y_train)
+
+	log_reg_model = cv.best_estimator_
+	print(log_reg_model)
+
+	# grid search CV for NN
+	params_nn = {'hidden_layer_sizes': [(3,), (5,), (7,), (9,)], 'alpha': [0.01,0.001, 0.0001, 0.00001]}
+
+	cv = GridSearchCV(param_grid=params_nn, estimator=MLPClassifier(max_iter=500), cv=10, n_jobs=-1)
+	cv.fit(X_train, y_train)
+
+	nn_model = cv.best_estimator_
+	print(nn_model)
+	
+	# test for accuracy
+	y_pred_dt = dt_model.predict(X_test)
+	y_pred_log_reg = log_reg_model.predict(X_test)
+	y_pred_nn = nn_model.predict(X_test)
+
+	print("Accuracy score on test for DT:", accuracy_score(y_test, y_pred_dt))
+	print("Accuracy score on test for logistic regression:", accuracy_score(y_test, y_pred_log_reg))
+	print("Accuracy score on test for NN:", accuracy_score(y_test, y_pred_nn))
+	
+	########################## ploting graphs ###############################
+	# typical prediction
+	y_pred = dt_model.predict(X_test)
+
+	# probability prediction from decision tree
+	y_pred_proba_dt = dt_model.predict_proba(X_test)
+
+	print("Probability produced by decision tree for each class vs actual prediction on TargetB (0 = non-donor, 1 = donor). You should be able to see the default threshold of 0.5.")
+	print("(Probs on zero)\t(probs on one)\t(prediction made)")
+	# print top 10
+	for i in range(10):
+		print(y_pred_proba_dt[i][0], '\t', y_pred_proba_dt[i][1], '\t', y_pred[i])
+		
+	from sklearn.metrics import roc_auc_score
+
+	y_pred_proba_dt = dt_model.predict_proba(X_test)
+	y_pred_proba_log_reg = log_reg_model.predict_proba(X_test)
+	y_pred_proba_nn = nn_model.predict_proba(X_test)
+
+	roc_index_dt = roc_auc_score(y_test, y_pred_proba_dt[:, 1])
+	roc_index_log_reg = roc_auc_score(y_test, y_pred_proba_log_reg[:, 1])
+	roc_index_nn = roc_auc_score(y_test, y_pred_proba_nn[:, 1])
+
+	print("ROC index on test for DT:", roc_index_dt)
+	print("ROC index on test for logistic regression:", roc_index_log_reg)
+	print("ROC index on test for NN:", roc_index_nn)
+	
+	fpr_dt, tpr_dt, thresholds_dt = roc_curve(y_test, y_pred_proba_dt[:,1])
+	fpr_log_reg, tpr_log_reg, thresholds_log_reg = roc_curve(y_test, y_pred_proba_log_reg[:,1])
+	fpr_nn, tpr_nn, thresholds_nn = roc_curve(y_test, y_pred_proba_nn[:,1])
+	
+	plt.plot(fpr_dt, tpr_dt, label='ROC Curve for DT {:.3f}'.format(roc_index_dt), color='red', lw=0.5)
+	plt.plot(fpr_log_reg, tpr_log_reg, label='ROC Curve for Log reg {:.3f}'.format(roc_index_log_reg), color='green', lw=0.5)
+	plt.plot(fpr_nn, tpr_nn, label='ROC Curve for NN {:.3f}'.format(roc_index_nn), color='darkorange', lw=0.5)
+
+	# plt.plot(fpr[2], tpr[2], color='darkorange',
+	#          lw=lw, label='ROC curve (area = %0.2f)' % roc_auc[2])
+	plt.plot([0, 1], [0, 1], color='navy', lw=0.5, linestyle='--')
+	plt.xlim([0.0, 1.0])
+	plt.ylim([0.0, 1.05])
+	plt.xlabel('False Positive Rate')
+	plt.ylabel('True Positive Rate')
+	plt.title('Receiver operating characteristic example')
+	plt.legend(loc="lower right")
+	plt.show()
 ########################### Global Function ###########################################
 def analyse_feature_importance(dm_model, feature_names, n_to_display=20):
     # grab feature importances from the model
